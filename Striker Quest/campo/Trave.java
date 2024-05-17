b090fd1313b25184cc609faa5ab8b6e9f6260c47
rafaelps7 <rafasuyeyassu@gmail.com>
2024-05-17 12:31:35 -0300

    Rafael

---
+++ Striker Quest/campo/Trave.java
@@ -0,0 +1,14 @@
+package campo;
+
+public class Trave {
+    public Double altura;
+    public Double largura;
+
+    public void modificarAltura(){
+        System.out.println(""); //"Insira a nova altura" ou "Altura modificada"? 
+    }
+    public void modificarLargura(){
+        System.out.println("");// "Insira a nova largura" ou "Largura"
+    }
+
+}
