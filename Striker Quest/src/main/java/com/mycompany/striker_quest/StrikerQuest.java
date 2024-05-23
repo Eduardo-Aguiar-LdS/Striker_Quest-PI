4a152542f4e5ebce21616f289d0fb0cb944170ad
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-05-23 17:37:56 -0300

    Criação da classe principal StrikerQuest

---
+++ Striker Quest/src/main/java/com/mycompany/striker_quest/StrikerQuest.java
@@ -0,0 +1,22 @@
+package com.mycompany.striker_quest;
+
+/**
+ * Classe StrikerQuest
+ *
+ * Esta é a principal classe utilizada no sistema do jogo StrikerQuest.
+ *
+ */
+public class StrikerQuest {
+
+    /**
+     * Método main
+     *
+     * Este é o método principal utilizado no sistema do jogo Striker Quest.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
